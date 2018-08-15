208afb385ce6bf20a2ef6a02b00b1c8a0b7ab369
Alyssa Thunberg <alyssa.thunberg@gmail.com>
2018-08-15 04:50:37 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,7 @@ board = [" "," "," "," "," "," "," "," "," ",]
 def position_taken?(board,index)
   if board[index] == " " || "" || nil
     false
+  elsif board[index] == "X" || "O"
+    true
   end
 end
